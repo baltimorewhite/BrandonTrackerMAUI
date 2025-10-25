1d89b75e1a2a3c732f22cff1e6f5a01b3cf5e136
baltimoreWhite <baltimorewhite@gmail.com>
2025-10-26 00:36:06 +0800

    chore: set navigation pages

--- BrandonTrackerApp/BrandonTrackerApp/AppShell.xaml.cs
+++ BrandonTrackerApp/BrandonTrackerApp/AppShell.xaml.cs
@@ -5,6 +5,12 @@
         public AppShell()
         {
             InitializeComponent();
+
+            Routing.RegisterRoute("home", typeof(Pages.HomePage));
+            Routing.RegisterRoute("tasks", typeof(Pages.TaskListPage));
+            Routing.RegisterRoute("history", typeof(Pages.TaskDetailPage));
+            Routing.RegisterRoute("settings", typeof(Pages.SettingsPage));
         }
     }
 }
+
