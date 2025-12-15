b0164bb0751d6744619e5072a2c6c97efe6c28b9
baltimoreWhite <baltimorewhite@gmail.com>
2025-12-15 20:52:23 +0800

    feat(ui): implement orientation-aware HomePage img toggle and bin summary grid

--- BrandonTrackerApp/BrandonTrackerApp/Pages/HomePage.xaml.cs
+++ BrandonTrackerApp/BrandonTrackerApp/Pages/HomePage.xaml.cs
@@ -2,8 +2,20 @@ namespace BrandonTrackerApp.Pages;
 
 public partial class HomePage : ContentPage
 {
-	public HomePage()
-	{
-		InitializeComponent();
-	}
+    public string CurrentDate => DateTime.Today.ToString("dd/MM/yyyy");
+
+    // Valores simulados (luego los conectamos al ViewModel)
+    public int Bin100L => 3;
+    public int Bin240L => 5;
+    public int BinCorporate => 2;
+    public int BinArchive => 1;
+    public int BinSmall => 4;
+    public int BinLocked => 0;
+
+    public HomePage()
+    {
+        InitializeComponent();
+        BindingContext = this;
+    }
 }
+
